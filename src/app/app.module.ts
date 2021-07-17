9f1b67224d746b5cd72f8d116cbf6356574dce65
tanmayb99 <tanmaybhargava9986@gmail.com>
2021-07-17 20:44:33 +0200

    header and homes components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './src/components/header/header.component';
+import { HomesComponent } from './src/components/homes/homes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomesComponent
   ],
   imports: [
     BrowserModule
